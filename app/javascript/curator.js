7132a20b3aacc4334a09588b7482d4ca1936e321
Andrew McWilliams <andy@jahya.net>
2019-05-15 16:00:43 -0400

    Switches to script-based method of loading curator social

--- app/javascript/curator.js
+++
@@ -1,5 +0,0 @@
-(function(){
-var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
-i.src = "https://cdn.curator.io/published/d4196ede-5a4b-4aaa-a284-2c32e3cbd6df.js";
-e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
-})();
---
+++ app/javascript/homepage-social.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	function init() {
+		if(window.location.pathname == '/') {
+			showSocialFeed();
+		}
+	}
+
+	function showSocialFeed() {
+		var widget = new Curator.Widgets.Waterfall({
+			container: '#curator-feed',
+			feedId: 'd4196ede-5a4b-4aaa-a284-2c32e3cbd6df',
+			postClickAction: 'goto-source',
+			postClickReadMoreAction: 'goto-source',
+			postsPerPage: 26,
+			filter: {
+				showNetworks: false,
+				showSources: false
+			}
+		});
+	}
+
+	init();
+});
